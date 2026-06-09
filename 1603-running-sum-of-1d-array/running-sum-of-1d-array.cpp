309ac3536538432811ba58d7fecdecf4ef0b15e7
katyayani29verma-cyber <katyayani29verma@gmail.com>
2026-06-09 15:16:26 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (50.96%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> out(nums.size());
+        out[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            out[i] = out[i-1] + nums[i];
+        }
+        return out;
+    }
+};
